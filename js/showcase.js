4adc286234ba54b780539959fe922f0ee017d9c2
Nina Lin <37187561+nina-lin@users.noreply.github.com>
2026-04-20 23:53:53 -0400

    Add files via upload

---
+++ js/showcase.js
@@ -0,0 +1,20 @@
+(function() {
+
+  let select = document.getElementById("select");
+
+  //load data
+  function init() {
+    console.log("Hello, World!")
+  
+  //  signalIframe(); }, 3500);
+
+  }
+
+
+  window.addEventListener('load', function(){
+
+    init(); 
+  })
+
+  
+})();
